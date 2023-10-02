7cb8f77a6d928000e488d68bb90db4477d4e28b3
RabiaSid <rabiasid984@gmail.com>
2023-10-02 19:34:57 +0500

    complete result section

--- src/screens/quiz/index.tsx
+++ src/screens/quiz/index.tsx
@@ -10,9 +10,10 @@ export default function UserQuiz() {
   const [isTrue, setIsTrue] = useState(true);
   const [model, setModel] = useState<any>({ secretInput: "" });
   const [answered, setAnswered] = useState<any>([]);
-  const [count, setCount] = useState<any>(1);
-  const [answerSelected, setAnswerSelected] = useState(false);
-  const [showSelected, setShowSelected] = useState(false);
+  const [count, setCount] = useState<any>(0);
+  const [answerSelected, setAnswerSelected] = useState<any>(false);
+  const [showSelected, setShowSelected] = useState<any>(false);
+  const [showModal, setShowModal] = useState<any>(false);
 
   const fillModel = (key: string, val: any) => {
     model[key] = val;
@@ -54,7 +55,12 @@ export default function UserQuiz() {
     setShowSelected(true);
   };
 
-  const showResult = () => {};
+  const showResult = () => {
+    setShowModal(!showModal);
+    setShowSelected(true);
+  };
+
+  const totalMarks = (questionList.length / count) * 10;
 
   return (
     <>
@@ -115,6 +121,39 @@ export default function UserQuiz() {
                     <Button onClick={showResult} label="Show Result" />
                   </div>
                 )}
+                {showModal ? (
+                  <>
+                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                      <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                          <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                            <h3 className="text-3xl font-semibold">
+                              Show Result
+                            </h3>
+                          </div>
+                          <div className="relative p-6 flex-auto">
+                            <h3 className="text-2xl my-4 text-slate-500 text-lg leading-relaxed px-8">
+                              point = {count}
+                            </h3>
+                            <h3 className="text-2xl my-4 text-slate-500 text-lg leading-relaxed px-8">
+                              Total marks = {totalMarks} / {count * 10}
+                            </h3>
+                          </div>
+                          <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                            <button
+                              className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                              type="button"
+                              onClick={showResult}
+                            >
+                              Save Changes
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                  </>
+                ) : null}
               </div>
             </div>
 
